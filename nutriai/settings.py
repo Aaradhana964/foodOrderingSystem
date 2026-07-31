2edd3a613ce06d990042c538ff46363ccd4a47c9
Aaradhana964 <prajapatiaaradhana65@gmail.com>
2026-07-31 20:41:15 +0530

    Add requirements.txt and update settings

--- nutriai/settings.py
+++ nutriai/settings.py
@@ -23,7 +23,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 DEBUG = os.getenv("DEBUG") == "True"
 
 ALLOWED_HOSTS = ["127.0.0.1",
-    "localhost",]
+    "localhost",".onrender.com"]
 
 
 # Application definition
